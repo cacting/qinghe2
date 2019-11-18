30fdc61f2a0bcab6a762153ffb0a235c3d70bbae
cacting <lt1477602761@163.com>
2019-11-18 22:04:47 +0800

    grid

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,5 +1,31 @@
 <template>
   <div>
+    <i-grid>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>欧美</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>日韩</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>港台</i-grid-label>
+    </i-grid-item>
+    <i-grid-item>
+        <i-grid-icon>
+            <image src="/static/images/user.png" />
+        </i-grid-icon>
+        <i-grid-label>大陆</i-grid-label>
+    </i-grid-item>
+</i-grid>
   </div>
 </template>
 
