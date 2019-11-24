24ae3b4e0f8bab99d5cf83f153ee970ea57a498a
cacting <lt1477602761@163.com>
2019-11-24 14:16:07 +0800

    index

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,27 +1,27 @@
 <template>
   <div>
-    <i-grid>
-    <i-grid-item>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
-            <image src="/static/images/user.png" />
+            <image src="/static/images/5.png" />
         </i-grid-icon>
         <i-grid-label>欧美</i-grid-label>
     </i-grid-item>
-    <i-grid-item>
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
-            <image src="/static/images/user.png" />
+            <image src="/static/images/6.png" />
         </i-grid-icon>
         <i-grid-label>日韩</i-grid-label>
     </i-grid-item>
-    <i-grid-item>
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
-            <image src="/static/images/user.png" />
+            <image src="/static/images/7.png" />
         </i-grid-icon>
         <i-grid-label>港台</i-grid-label>
     </i-grid-item>
-    <i-grid-item>
+    <i-grid-item i-class="no-border">
         <i-grid-icon>
-            <image src="/static/images/user.png" />
+            <image src="/static/images/8.png" />
         </i-grid-icon>
         <i-grid-label>大陆</i-grid-label>
     </i-grid-item>
@@ -45,4 +45,10 @@ export default {
 </script>
 
 <style scoped>
+div >>> .no-border {
+    border-width:0pt;
+}
+div >>> .split{
+    margin-bottom:10pt;
+}
 </style>
