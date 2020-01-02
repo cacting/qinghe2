0ac524c7a61771c764e11940e5996ae435b91082
cacting <lt1477602761@163.com>
2020-01-03 01:38:24 +0800

    layout1

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -8,8 +8,9 @@
     </view>
     <i-panel title="来创建属于自己的歌单吧">
       <i-input :value="name" @change="changeName($event)" title="歌单名称" autofocus placeholder="请输入名称" maxlength="20" />
+      <i-input :value="name" @change="changeName($event)" title="简介" autofocus placeholder="请输入简介" maxlength="20" />
     </i-panel>
-    <i-button @click="handleClick" type="warning" size="default">我要添加</i-button>
+    <i-button @click="handleClick" type="warning" size="default">添加歌单</i-button>
     <view class="tips">每个人都应该有一个属于自己的歌单</view>
   </div>
 </template>
---
+++ src/pages/recommend/index.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <i-grid i-class="no-border">
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+          <image src="/static/images/5.png" />
+      </i-grid-icon>
+      <i-grid-label>书香古韵</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+          <image src="/static/images/6.png" />
+      </i-grid-icon>
+      <i-grid-label>拼搏生活</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+          <image src="/static/images/7.png" />
+      </i-grid-icon>
+      <i-grid-label>腊八年味</i-grid-label>
+    </i-grid-item>
+    </i-grid>      
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {             
+    }
+  },
+
+  methods: {     
+  },
+
+  created () {      
+  }
+}
+</script>
+
+<style scoped>
+div >>> .no-border {
+    border-width:0pt;
+}
+div >>> .split{
+    margin-bottom:10pt;
+}
+</style>
+
+
