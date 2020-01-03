0ca646a82729deae528958ddea4418c8afb55cfc
cacting <lt1477602761@163.com>
2020-01-03 15:45:54 +0800

    layout2

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,38 +1,55 @@
 <template>
   <div>
-      <swiper
+    <swiper
       :indicator-dots="indicatorDots"
       :autoplay="autoplay"
       :interval="interval"
       :duration="duration"
       style="height:200px">
-    <block v-for="item in imgUrls" :key="item">            
-      <swiper-item>
-        <image :src="item" style="width:100%;"/>
-      </swiper-item>
-    </block>
-  </swiper>
-    <i-grid i-class="no-border">
-        <i-grid-item @click="goList(item.url)" i-class="no-border" v-for="item in grids" :key="item">
-            <i-grid-icon>
-             <image :src="item.image" />
-         </i-grid-icon>
-         <i-grid-label>{{item.type}}</i-grid-label>
-        </i-grid-item>
-        <i-grid-item i-class="no-border">
-            <i-grid-icon>
-                <image src="/static/images/6.png" />
-            </i-grid-icon>            
-        </i-grid-item>
+      <block v-for="item in imgUrls" :key="item">            
+        <swiper-item>
+          <image :src="item" style="width:100%;"/>
+        </swiper-item>
+      </block>
+    </swiper>
+    <i-grid>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+          <image src="/static/images/5.png" />
+      </i-grid-icon>
+      <i-grid-label>书香古韵</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+          <image src="/static/images/6.png" />
+      </i-grid-icon>
+      <i-grid-label>拼搏生活</i-grid-label>
+    </i-grid-item>
+    <i-grid-item i-class="no-border">
+        <i-grid-icon>
+          <image src="/static/images/7.png" />
+      </i-grid-icon>
+      <i-grid-label>腊八年味</i-grid-label>
+    </i-grid-item>
     </i-grid>
 
-    <i-panel :title="title_name">
-        <view style="padding: 15px;">
-            <i-card v-for="item in top" :key="item" i-class="split" :title="item.name" thumb="item.jpg">
-                <view slot="content">{{item.singer}}</view>
-                <view slot="footer">{{item.time}}</view>
-            </i-card>           
-        </view>
+    <i-panel title="猜你喜欢">
+        <view style="padding: 10px;">
+           <i-cell-group>             
+              <i-cell title="下山" is-link url="/pages/index"></i-cell>                            
+            </i-cell-group>
+            <i-cell-group>
+              <i-cell title="句号" is-link url="/pages/index"></i-cell>             
+            </i-cell-group>
+            <i-cell-group>
+              <i-cell title="透明" is-link url="/pages/index"></i-cell>
+            </i-cell-group>
+            <i-cell-group>
+              <i-cell title="关闭推荐">
+                  <switch slot="footer" checked />
+              </i-cell>
+            </i-cell-group>
+        </view>                                      
     </i-panel>
   </div>
 </template>
@@ -40,20 +57,7 @@
 <script>
 export default {
   data () {
-    return {
-        title_name: "推荐歌单",
-        grids:[
-            {title:"红歌",image:"/static/images/5.png"},
-            {title:"流行",image:"/static/images/6.png"},
-            {title:"古典",image:"/static/images/7.png"},
-            {title:"摇滚",image:"/static/images/8.png"},
-        ],
-        top:[
-            {name:"歌名1",singer:"歌手1",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:"时长1"},
-            {name:"歌名2",singer:"歌手2",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:"时长2"},
-            {name:"歌名3",singer:"歌手3",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:"时长3"},
-            {name:"歌名4",singer:"歌手4",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:"时长4"}
-        ],
+    return {        
         imgUrls: [
         'http://www.canyin88.com/uploads/image/2019/04/16/1555378220872933.jpg',
         'http://www.canyin88.com/uploads/image/2019/04/15/1555321255843942.jpg',
--- src/pages/recommend/index.vue
+++ src/pages/recommend/index.vue
@@ -1,36 +1,36 @@
 <template>
   <div>
-    <i-grid i-class="no-border">
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-          <image src="/static/images/5.png" />
-      </i-grid-icon>
-      <i-grid-label>书香古韵</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-          <image src="/static/images/6.png" />
-      </i-grid-icon>
-      <i-grid-label>拼搏生活</i-grid-label>
-    </i-grid-item>
-    <i-grid-item i-class="no-border">
-        <i-grid-icon>
-          <image src="/static/images/7.png" />
-      </i-grid-icon>
-      <i-grid-label>腊八年味</i-grid-label>
-    </i-grid-item>
-    </i-grid>      
+    <i-panel :title="title_name">
+        <view style="padding: 15px;">
+            <i-card v-for="item in top" :key="item" i-class="split" :title="item.name" thumb="item.jpg">
+                <view slot="content">{{item.singer}}</view>
+                <view slot="footer">{{item.time}}</view>
+            </i-card>           
+        </view>
+    </i-panel>   
   </div>
 </template>
 
 <script>
 export default {
   data () {
-    return {             
+    return {
+      title_name: "推荐歌单",       
+        top:[
+            {name:"流行指数榜",singer:"1 透明",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:""},
+            {name:"热歌榜",singer:"1 下山",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:""},
+            {name:"新歌榜",singer:"1 句号",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:""},
+            {name:"飙升榜",singer:"1 火红的萨日朗",image:"https://i.loli.net/2017/08/21/599a521472424.jpg",time:""}
+        ],             
     }
   },
 
-  methods: {     
+  methods: {
+      goType(type){
+          console.log(type)
+          let url = '../list/main?type=' +type.title
+          mpvue.navigateTo({ url })
+      }     
   },
 
   created () {      
