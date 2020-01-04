591d315679f5fc9a146e02f2ce818753fc72bc80
cacting <lt1477602761@163.com>
2020-01-04 22:31:17 +0800

    show

--- src/pages/list/index.vue
+++ src/pages/list/index.vue
@@ -3,9 +3,9 @@
     <image style="height:200px" i-class="split" src="/static/images/bj1.jpg" />
     <i-panel title=" ">
       <view>
-        <i-card i-class="split" v-for="item in top" :key="item" :extra="item.name" :thumb="item.img">
-            <view slot="content">歌手{{item.singer}}</view>
-            <view slot="footer">专辑{{item.album}}</view>
+        <i-card i-class="split" style="width:100%" v-for="item in top" :key="item" :extra="item.name" :thumb="item.img">
+            <view slot="content">歌手·{{item.singer}}</view>
+            <view slot="footer">专辑·{{item.album}}</view>
         </i-card>
       </view>
     </i-panel>
